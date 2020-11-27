cc9abbbabed1b140402ac3a24c433eac97e06649
HarishSurya <38115933+HarishSurya@users.noreply.github.com>
2020-11-27 13:40:15 -0500

    Update App.js

--- ajmane/frontend/src/App.js
+++ ajmane/frontend/src/App.js
@@ -127,6 +127,16 @@ function App() {
           </ul>
         </aside>
         
+<div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+
         <main>
           <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen} exact></Route>
