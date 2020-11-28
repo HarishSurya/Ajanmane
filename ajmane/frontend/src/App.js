81d376fd35b157070663b55f10b3684a3d369d7d
HarishSurya <38115933+HarishSurya@users.noreply.github.com>
2020-11-28 14:15:15 -0500

    Update App.js

--- ajmane/frontend/src/App.js
+++ ajmane/frontend/src/App.js
@@ -49,14 +49,15 @@ function App() {
             </Link>
           </div>
     
-    /*<div className="header__search">
+    {/*
+        <div className="header__search">
  
         <input className="header__searchInput" type="text" />
       <div className="searchbtn">
         <button type="submit">Search</button>
       </div>
       </div>
-    */
+    */}
     
           <div>
             <Link to="/cart">
