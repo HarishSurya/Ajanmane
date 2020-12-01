ea84705e46c2f43ea3884af0af7e60bf4830b192
HarishSurya <38115933+HarishSurya@users.noreply.github.com>
2020-12-01 11:05:21 -0500

    Update App.js

--- ajmane/frontend/src/App.js
+++ ajmane/frontend/src/App.js
@@ -50,15 +50,7 @@ function App() {
           </div>
     
     
-    <div className="home">
-      <div className="home__container">
-        <img
-          className="home__image"
-          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
-          alt=""
-        />
-      </div>
-    </div>
+    
     {/*
         <div className="header__search">
  
@@ -122,6 +114,23 @@ function App() {
             )}
           </div>
         </header>
+
+
+
+<div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+
+
+
+
+
 
         <aside className="sidebar">
           <h3>Shopping Categories</h3>
